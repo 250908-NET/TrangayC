9592b136e969ce64a146c9c1fce008d72259128e
Charles <charlestrangay@gmail.com>
2025-09-15 11:21:29 -0400

    added Task Model

---
+++ task_management_api/model/Task.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public enum PriorityLevel
+{
+    Low,
+    Medium,
+    High,
+    Critical
+}
+
+public class Task
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required] 
+    [MaxLength(100)] 
+    public string Title { get; set; }
+    [MaxLength(500)]
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; } = false;
+    public PriorityLevel Priority { get; set; }
+    public DateTime? DueDate { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    public Task(int id, string title, string description, bool isCompleted, PriorityLevel priority, DateTime dueDate, DateTime createdAt, DateTime updatedAt) {
+        this.Id = id;
+        this.Title = title;
+        this.Description = description;
+        this.IsCompleted = isCompleted;
+        this.Priority = priority;
+        this.DueDate = dueDate;
+        this.CreatedAt = createdAt;
+        this.UpdatedAt = updatedAt;
+    }
+}
